cc3a417f08e4558f1713180974ebb22b7aff32a3
Manisha <manisha.malla@gmail.com>
2016-08-22 20:13:32 +0530

    Add Unit Test for Voting component button rendering

--- src/components/Voting.js
+++ src/components/Voting.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 export default React.createClass({
 	render: function () {
-		console.log("In Voting component");
 		return <div className='voting'>
 				{this.props.pair.map(entry => 
 					<button key={entry}> 
