16a1870698ea7a67afcd4c9e8028910c30f5a849
Manisha <manisha.malla@gmail.com>
2016-08-23 11:46:51 +0530

    Plug-in React router for routes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Voting from './components/Voting';
+import {Router, Route, hashHistory} from 'react-router';
+import App from './components/App';
 
-const pair = ['The Terminal', 'Forrest Gump'];
+var routes = (
+    <Router history={hashHistory}>
+        <Route component={App}>
+            <Route path='/' component={Voting} />
+        </Route>
+    </Router>
+);
 
 export default ReactDOM.render(
-	<Voting 
-		pair={pair} 
-		winner="Forrest Gump"/>,
+	routes,
 	document.getElementById('app')
 );
