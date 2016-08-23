c30ec5b73572122460e3f6308477f42d76ab33ad
Manisha <manisha.malla@gmail.com>
2016-08-23 11:25:44 +0530

    Add Unit Tests for Voting and Winner UI

--- test/components/Voting_spec.js
+++ test/components/Voting_spec.js
@@ -39,4 +39,46 @@ describe('Voting', () => {
 		expect(votedWith).to.equal("Forrest Gump");
 		
 	});
+
+	it('disables buttons when voting is done', () => {
+		const component = renderIntoDocument(
+			<Voting 
+				pair={["The Terminal", "Forrest Gump"]} 
+				hasVoted="Forrest Gump"	/>
+		);
+		const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+		
+		expect(buttons.length).to.equal(2);
+		expect(buttons[0].hasAttribute('disabled')).to.equal(true);
+		expect(buttons[1].hasAttribute('disabled')).to.equal(true);
+		
+
+	});
+
+	it('adds label to the voted entry', () => {
+		const component = renderIntoDocument(
+			<Voting 
+				pair={["The Terminal", "Forrest Gump"]} 
+				hasVoted="Forrest Gump"	/>
+		);
+		const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+		
+		expect(buttons[1].textContent).to.contain("Voted");
+	});
+
+	it('renders just the winner when there is one', () => {
+		const component = renderIntoDocument(
+			<Voting 
+				pair={["The Terminal", "Forrest Gump"]} 
+				winner="Forrest Gump"	/>
+		);
+		const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+		
+		expect(buttons.length).to.equal(0);
+		const winner = ReactDOM.findDOMNode(component.refs.winner);
+
+		expect(winner).to.be.ok;
+		expect(winner.textContent).to.contain('Forrest Gump');
+
+	});
 });
