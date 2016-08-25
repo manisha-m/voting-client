7927de512a1937a9bf1253b94864f0f11b8bdad7
Manisha <manisha.malla@gmail.com>
2016-08-25 11:38:56 +0530

    Add Results route to react router

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import Voting from './components/Voting';
 import {Router, Route, hashHistory} from 'react-router';
 import App from './components/App';
+import Results from './components/Results';
 
 var routes = (
     <Router history={hashHistory}>
         <Route component={App}>
-            <Route path='/' component={Voting} />
+            <Route path='/results' component={Results}/>
+            <Route path='/' component={Voting}/>
         </Route>
     </Router>
 );
