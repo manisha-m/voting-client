1d20a3c17429bf9ca9d2e545d0ae6e3b75ead957
Manisha <manisha.malla@gmail.com>
2016-08-25 10:27:13 +0530

    Add PureRenderMixin for faster react diffs

--- src/components/Vote.js
+++ src/components/Vote.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
 
 export default React.createClass({
+	mixins: [PureRenderMixin],
 	isDisabled: function () {
 		return !!this.props.hasVoted;
 	},
