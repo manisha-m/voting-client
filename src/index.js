86ea754ef1a64043b8bd4c450e561c57aaa26f0f
Manisha <manisha.malla@gmail.com>
2016-08-31 20:42:19 +0530

    Use redux middleware to send vote action to server

--- src/index.js
+++ src/index.js
@@ -4,19 +4,25 @@ import {VotingContainer} from './components/Voting';
 import {Router, Route, hashHistory} from 'react-router';
 import App from './components/App';
 import {ResultsContainer} from './components/Results';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import reducer from './reducer';
 import {setState} from './action_creators';
 import io from 'socket.io-client';
-
-const store = createStore(reducer);
+import remoteActionMiddleware from './remote_action_middleware';
 
 console.log("About to open socket.io client connection");
+console.log(`${location.protocol}//${location.hostname}:8090`);
 const socket = io(`${location.protocol}//${location.hostname}:8090`);
 socket.on('state', state => 
 	store.dispatch(setState(state))
 );
+
+const createStoreWithMiddleware = applyMiddleware(
+  remoteActionMiddleware(socket)
+)(createStore);
+const store = createStoreWithMiddleware(reducer);
+
 
 const routes = 
         <Route component={App}>
